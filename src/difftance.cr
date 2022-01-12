3d821c1a1e4a8d3073cba4bc998c932c5b661a95
fabon <syobon.hinata.public@gmail.com>
2022-01-12 19:44:47 +0900

    Implement edit distance calculation

--- src/difftance.cr
+++ src/difftance.cr
@@ -7,12 +7,16 @@ end
 
 
 args = [] of String
+no_sub = false
 
-OptionParser.parse do |parser|
+parser = OptionParser.parse do |parser|
   parser.banner = "Usage: difftance [options] FILE1 FILE2"
   parser.on("-h", "--help", "Show this help") do
     puts parser
     exit
+  end
+  parser.on("--no-substitution", "Disable substitution(fast, allow only deletion and insertion)") do
+    no_sub = true
   end
 
   parser.invalid_option do |flag|
@@ -24,4 +28,25 @@ OptionParser.parse do |parser|
   parser.unknown_args do |unknown_args|
     args = unknown_args
   end
+end
+
+if args.size == 7 && ENV.has_key?("GIT_DIFF_PATH_COUNTER")
+  path, old_file, old_hex, old_mode, new_file, new_hex, new_mode = args
+  old_content = File.read(File.expand_path(old_file))
+  new_content = File.read(File.expand_path(new_file))
+
+  distance = no_sub ? Difftance::EditDistance.edit_distance_no_substitution(old_content, new_content) : Difftance::EditDistance.dp(old_content, new_content)
+  puts "#{path}: #{distance}"
+elsif args.size == 1 && ENV.has_key?("GIT_DIFF_PATH_COUNTER")
+  path = args[0]
+  puts "#{path}: #{File.read(File.expand_path(path)).size}"
+elsif args.size == 2
+  content1 = File.read(args[0])
+  content2 = File.read(args[1])
+  distance = no_sub ? Difftance::EditDistance.edit_distance_no_substitution(content1, content2) : Difftance::EditDistance.dp(content1, content2)
+  puts "#{args[0]}, #{args[1]}: #{distance}"
+else
+  STDERR.puts "Error: Invalid arguments"
+  STDERR.puts parser
+  exit 1
 end
---
+++ src/difftance/edit_distance.cr
@@ -0,0 +1,68 @@
+module Difftance
+  module EditDistance
+    extend self
+
+    # Wagner–Fischer algorithm
+    def dp(before before_str : String, after after_str : String, scores = { deletion: 1, insertion: 1, substitutions: 1 })
+      before_chars = before_str.chars
+      after_chars = after_str.chars
+      ins = scores[:insertion]
+      del = scores[:deletion]
+      sub = scores[:substitutions]
+      dp = Array.new(before_chars.size + 1) { |i| i == 0 ? Array.new(after_chars.size + 1) { |j| j * ins } : Array.new(after_chars.size + 1) { |j| j == 0 ? i * del : 0 } }
+      (1..before_chars.size).each do |i|
+        (1..after_chars.size).each do |j|
+          a = dp[i][j - 1] + ins
+          b = dp[i - 1][j] + del
+          c = dp[i - 1][j - 1] + (before_chars[i - 1] == after_chars[j - 1] ? 0 : sub)
+          dp[i][j] = a > b ? (b > c ? c : b) : (a > c ? c : a)
+        end
+      end
+
+      dp.last.last
+    end
+
+    # Wu, S., Manber, U., Myers, E.W., & Miller, W. (1990). An O(NP) Sequence Comparison Algorithm. Inf. Process. Lett., 35, 317-323.
+    # https://api.semanticscholar.org/CorpusID:9968782
+    # ins=1, del=1, sub=2(disallowing substitution)
+    def edit_distance_no_substitution(str1 : String, str2 : String)
+      chars1, chars2 = str1.size < str2.size ? {str1.chars, str2.chars} : {str2.chars, str1.chars}
+
+      delta = chars2.size - chars1.size
+      offset = chars1.size + 1
+      dd = delta + offset
+
+      fp = Array.new(chars1.size + chars2.size + 3) { -1 }
+
+      p = 0
+      while fp[dd] != chars2.size
+        (-p).upto(delta - 1) do |k|
+          kk = k + offset
+          v0 = fp[kk - 1] + 1
+          v1 = fp[kk + 1]
+          fp[kk] = snake(k, (v0 > v1 ? v0 : v1), chars1, chars2)
+        end
+        (delta + p).downto(delta + 1) do |k|
+          kk = k + offset
+          v0 = fp[kk - 1] + 1
+          v1 = fp[kk + 1]
+          fp[kk] = snake(k, (v0 > v1 ? v0 : v1), chars1, chars2)
+        end
+        v0 = fp[dd - 1] + 1
+        v1 = fp[dd + 1]
+        fp[dd] = snake(delta, (v0 > v1 ? v0 : v1), chars1, chars2)
+        p += 1
+      end
+      delta + (p - 1) * 2
+    end
+
+    private def snake(k : Int, y : Int, str1 : Array(Char), str2 : Array(Char))
+      x = y - k
+      while x < str1.size && y < str2.size && str1[x] == str2[y]
+        x += 1
+        y += 1
+      end
+      y
+    end
+  end
+end
